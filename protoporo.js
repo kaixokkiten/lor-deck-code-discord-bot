cc01925f760240366536b9640d1728d2698058c1
kaixokkiten <kaixokkiten@gmail.com>
2021-09-04 00:12:07 -0700

    ordered output by mana cost, added mana cost and deck code to output

--- protoporo.js
+++ protoporo.js
@@ -1,10 +1,13 @@
 const discord = require('discord.js');
+const {appendFileSync} = require('fs');
 
 const set1 = require('./set1-en_us.json');
 const set2 = require('./set2-en_us.json');
 const set3 = require('./set3-en_us.json');
 const set4 = require('./set4-en_us.json');
-const sets = [set1, set2, set3, set4];
+const set5 = require('./set5-en_us.json');
+
+const sets = [set1, set2, set3, set4, set5];
 
 var auth = require('./auth.json');
 const lor = require('lor-deckcodes-ts');
@@ -13,22 +16,25 @@ const client = new discord.Client({ intents: [discord.Intents.FLAGS.GUILDS, disc
 
 // Initialize Discord Bot
 client.on('message', message => {
-    console.log(message);
-    console.log("pong");
-    if (message.content === "ping") message.channel.send("pong");
+    console.log(message.content);
+    if (message.content === "[ping]") {
+        message.channel.send("pong");
+        console.log("pong");
+    }
 });
 
 client.on('message', message => {
-    console.log(message);
-    console.log("pong");
     let deckCode = message.content.matchAll(/\[([A-Z0-9]+)\]/g).next();
     if (deckCode.value) {
-        let deck = (lor.getDeckFromCode(deckCode.value[1])).map(e =>"- "+e.count+" "+sets[+e.cardCode.slice(0, 2)-1].find(card => e.cardCode == card.cardCode).name);
-        message.channel.send(deck.join("\n"));
-    }
+        console.log(lor.getDeckFromCode(deckCode.value[1]));
+        let deck = (lor.getDeckFromCode(deckCode.value[1]))
+            .map(e => ({card:sets[+e.cardCode.slice(0, 2)-1].find(card => e.cardCode == card.cardCode), count:e.count}))
+            .sort((a, b) => b.card.cost - a.card.cost)
+            .map(e =>"- "+e.count+" "+"("+e.card.cost+") "+e.card.name);
+        let decklist = deck.join("\n");
 
+        message.channel.send(deckCode.value[1]+"\n"+decklist);
+    }
 });
 
 client.login(auth.token);
-
-
