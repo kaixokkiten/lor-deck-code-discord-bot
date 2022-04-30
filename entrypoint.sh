18bc5c518c2cd1dbf14da954c264968acd6fa38e
kuilin <kuilin@gmail.com>
2022-04-29 22:50:00 -0400

    Automatically download sets

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,11 @@ set -euxo pipefail
 
 cp -rT /app/src /app
 cd /app
+
+for s in set1 set2 set3 set4 set5; do
+  wget https://dd.b.pvp.net/latest/${s}-lite-en_us.zip -q -O /tmp/set.zip
+  unzip -j -o /tmp/set.zip en_us/data/${s}-en_us.json
+done
+
 npm ci
 node protoporo.js
